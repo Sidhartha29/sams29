d890ecf79b92a1e1d055a425e2b695699f63a452
Sidhartha Kalva <sidharthakalva@gmail.com>
2026-03-16 22:45:49 +0530

    Use VITE_BASE_URL environment variable for API

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API = axios.create({ baseURL: import.meta.env.VITE_BASE_URL || "http://localhost:5000/api" });
 
 // JWT removed - no auth header
 API.interceptors.request.use((config) => config);
